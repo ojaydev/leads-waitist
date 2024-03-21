efe39415838cf7cfe7194d17ada404104cfd2d18
Ojay Dev <123231157+ojaydev@users.noreply.github.com>
2024-03-20 17:26:20 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="md:h-full h-80 bg-[#C9F9D1] relative overflow-hidden">
           <Image
-            src="/photo_2024-01-28_10-14-35.jpg"
+            src="/logo.png"
             alt="Luminar Leads eBook"
             fill
             className="object-contain mt-8 md:mt-24 px-14 object-bottom"
